69b0e0ff40e02817b0bff2f858ebd9b8db357753
Sylvrleef <sylvrleef@gmail.com>
2018-03-10 10:52:15 -0600

    Created login page elements

---
+++ traxsystem/src/components/login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import UsernameInput from './components/UsernameInput';
+import PasswordInput from './components/PasswordInput';
+import ResetPassword from './components/ResetPassword';
+import RequestAccess from './components/RequestAccess';
+
+
+const App = () => {
+        return ( 
+            <div>
+                <NavBar />
+                <h1>LOGIN</h1>
+                <hr></hr>
+                Welcome message goes here!
+                <UsernameInput />
+                <PasswordInput />
+                <ResetPassword />
+                <RequestAccess />
+            </div>
+            );
+        };
+
+ReactDOM.render( < App / > , document.querySelector(".container"));
