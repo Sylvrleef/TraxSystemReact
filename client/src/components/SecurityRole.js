0b0d2ecb08221524b748ded2c6dab4fe62bbe881
Sylvrleef <sylvrleef@gmail.com>
2018-03-18 22:06:53 -0500

    Added components for login page, admin page, and initiator page

---
+++ client/src/components/SecurityRole.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SecurityRole extends Component {
+    render() {
+        return (
+            <div>
+                Security Role
+            </div>
+        );
+    }
+}
+
+export default SecurityRole;
