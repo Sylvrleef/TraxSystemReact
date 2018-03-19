c17f170901a69ec62adea05beb7ce88ff8e496b5
Sylvrleef <sylvrleef@gmail.com>
2018-03-19 15:17:29 -0500

    Added needed elements for Add Vendor Request

--- client/src/components/AddVendorRequestPage/addVendorRequest.js
+++ client/src/components/AddVendorRequestPage/addVendorRequest.js
@@ -12,14 +12,19 @@ import InitiatorNavbar from './components/InitiatorNavbar';
 const App = () => {
         return ( 
             <div>
-                <InitiatorNavbar />
-                <h1>Initiator Home Page</h1>
-                <hr></hr>
-                <HelloMsg />
-                <SecurityRole />
-                <RequestTable />
-                <ViewBtn /> 
-                <CancelBtn />
+                form - 
+                    * vendor name input required
+                    * address line 1 input required
+                    * address line 2 input optional
+                    * city input required
+                    * state dropdown required
+                    * zip code input required
+                    * phone input required format
+                    * contact name input required
+                    * email input optional format
+                    * website input optional
+                submit button
+                cancel button
             </div>
             );
         };
