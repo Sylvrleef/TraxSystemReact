2945829b5953ab3df655993065c5869341b26f52
Jason Saenz <jsigns@gmail.com>
2018-03-19 16:31:34 -0500

    add vendor request page done

--- client/src/components/AddVendorPage/addVendor.js
+++ client/src/components/AddVendorPage/addVendor.js
@@ -24,4 +24,4 @@ const App = () => {
             );
         };
 
-ReactDOM.render( < App / > , document.querySelector(".container"));
+ReactDOM.render( < App /> , document.querySelector(".container"));
