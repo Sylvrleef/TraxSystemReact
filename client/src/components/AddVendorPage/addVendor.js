6e32ac9a66156ed78065ca259cc006edc6970a37
Sylvrleef <sylvrleef@gmail.com>
2018-03-19 15:53:32 -0500

    Added description of elements for the add vendor js file

--- client/src/components/AddVendorPage/addVendor.js
+++ client/src/components/AddVendorPage/addVendor.js
@@ -1,27 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import ViewBtn from './components/ViewBtn';
-import CancelBtn from './components/CancelBtn';
-import RequestTable from './components/RequestTable';
-import SecurityRole from './components/SecurityRole';
-import HelloMsg from './components/HelloMsg';
-import InitiatorNavbar from './components/InitiatorNavbar';
-
-
-const App = () => {
-        return ( 
-            <div>
-                <InitiatorNavbar />
-                <h1>Initiator Home Page</h1>
-                <hr></hr>
-                <HelloMsg />
-                <SecurityRole />
-                <RequestTable />
-                <ViewBtn /> 
-                <CancelBtn />
-            </div>
-            );
-        };
-
-ReactDOM.render( < App / > , document.querySelector(".container"));
+// form - 
+//     * vendor name input required
+//     * address line 1 input required
+//     * address line 2 input optional
+//     * city input required
+//     * state dropdown required
+//     * zip code input required
+//     * phone input required format
+//     * contact name input required
+//     * email input optional format
+//     * website input optional
+// submit button
+// cancel button
