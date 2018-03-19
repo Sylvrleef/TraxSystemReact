195b61a14fc0fd59485e7c323479090f1bb16dbb
Jason Saenz <jsigns@gmail.com>
2018-03-19 15:18:42 -0500

    login state updated

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-import UsernameInput from '../components/LoginPage/UsernameInput';
-import PasswordInput from '../components/LoginPage/PasswordInput';
-import ResetPassword from '../components/LoginPage/ResetPassword';
-import RequestAccess from '../components/LoginPage/RequestAccess';
+import { UsernameInput } from "../components/LoginPage/UsernameInput";
+import { PasswordInput } from "../components/LoginPage/PasswordInput";
+import ResetPassword from "../components/LoginPage/ResetPassword";
+import RequestAccess from "../components/LoginPage/RequestAccess";
 import HelloMsg from "../components/HelloMsg";
 
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      password: ""
+    };
+  }
 
-const Login = () => {
-        return ( 
-            <div>
-                <h1>LOGIN</h1>
-                <hr></hr>
-                <HelloMsg />
-                <UsernameInput />
-                <PasswordInput />
-                <ResetPassword />
-                <RequestAccess />
-            </div>
-            );
-        };
-
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this[name] = value;
+    this.setState({
+      [name]: value
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>LOGIN</h1>
+        <hr />
+        <form>
+          <HelloMsg />
+          <UsernameInput
+            type="text"
+            name="userName"
+            onChange={this.handleInputChange}
+            value={this.state.userName}
+          />
+          <PasswordInput
+            name="password"
+            onChange={this.handleInputChange}
+            value={this.state.password}
+          />
+          <ResetPassword />
+          <RequestAccess />
+        </form>
+      </div>
+    );
+  }
+}
 export default Login;
