d1f12958dde4bfe15cebec6ccb1e2d6f98f74d2e
Sylvrleef <sylvrleef@gmail.com>
2018-03-19 16:01:15 -0500

    Updated add vendor js file description for submit and cancel buttons

--- client/src/components/AddVendorPage/addVendor.js
+++ client/src/components/AddVendorPage/addVendor.js
@@ -9,5 +9,6 @@
 //     * contact name input required
 //     * email input optional format
 //     * website input optional
-// submit button
-// cancel button
+// submit button - modal pops up say your request has been sent to admin for approval. 
+//                  Ok button on modal returns user to home page
+// cancel button - returns to user home page without saving anything
