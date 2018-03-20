a0abc24b952dab3e4e103cae6c3a5ea925162e09
arodg <anitarodgerson@gmail.com>
2018-03-20 18:43:49 -0500

    Modify model

--- models/item.js
+++ models/item.js
@@ -1,40 +1,24 @@
 var Sequelize = require("sequelize");
 
 module.exports = function(sequelize, DataTypes) {
-  var item = sequelize.define("items", {
+  var item = sequelize.define("item", {
+    
     item_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-      options: { initialAutoIncrement: 100000 }
+      primaryKey: true
     },
 
     item_name: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-
-    item_quant: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-
-    item_unit: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false
-    },
-
-    request_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true
     }
-  });
 
+    }, {
+      freezeTableName: true,
+      underscored: true
+    }
+  );
+    
   return item;
 };
