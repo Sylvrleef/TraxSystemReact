36d091b4c4f3dd47b55b34fa69523dd29f44a285
arodg <anitarodgerson@gmail.com>
2018-03-20 17:47:37 -0500

    Modify server

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
-const models = require("./models");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const path = require("path");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Serve up static assets (usually on heroku)
+//Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.post("/api/items", function(req, res) {
-  console.log(req.body);
-  models.items
-    .create(req.body)
-    .then(() => console.log("success"))
-    .catch(err => console.log(err));
-});
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-models.sequelize.sync().then(() => {
-  app.listen(PORT, function() {
-    console.log(`🌎 ==> Server now on port ${PORT}!`);
+// Add routes
+app.use(routes);
+
+
+// Sync sequelize models
+const db = require("./models");
+db.sequelize.sync({force: false}).then(function() {
+  app.listen(PORT,function() {
+    console.log("App now listening at localhost:" + PORT);
   });
 });
